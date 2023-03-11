5494e25ca2271e88c745bab8d73bc53da84cf221
Hau Vong <hvong@pmc.com>
2023-03-10 19:34:38 -0800

    Edit UI for notes

--- client/src/components/ExpandedRowComponent.js
+++ client/src/components/ExpandedRowComponent.js
@@ -1,8 +1,9 @@
 import {format} from 'date-fns';
 import {logError} from '../services/log';
+import {useEffect, useRef, useState} from 'react';
 
 export const useExpandedRowComponent = ({onUpdate, user}) => {
-    const expandedRowComponent = ({data}) => {
+    const ExpandedRowComponent = ({data}) => {
         const handleClick = ({target}) => {
             const action = target.getAttribute('action');
             const value = target.getAttribute('value');
@@ -18,8 +19,8 @@ export const useExpandedRowComponent = ({onUpdate, user}) => {
                     });
                 })
                 .catch((e) => {
-                    // @TODO
-                    logError(e);
+                    setErrorMessage('Error saving data, see console error log for details.');
+                    logError(`Error sending: /api/action/${action}/${value}`, e);
                 })
         };
         const ActionButton = ({id, label}) => {
@@ -37,23 +38,76 @@ export const useExpandedRowComponent = ({onUpdate, user}) => {
         const showClaim = !user || !data.reviewers?.includes(user?.displayName);
         const showApprove = !user || !data.approvers?.includes(user?.displayName);
         const showChange = !user || !data.requestChanges?.includes(user?.displayName);
+        const [editing, setEditing] = useState(false);
+        const [errorMessage, setErrorMessage] = useState(false);
+        const dirtyData = {...data};
+
+        const handleSaveClicked = ({target}) => {
+            if (JSON.stringify(dirtyData) !== JSON.stringify(data)) {
+                const value = target.getAttribute('value');
+                Object.assign(data, dirtyData);
+                target.parentElement.childNodes.forEach((el) => el.setAttribute('disabled', true));
+                fetch(`/api/action/save/${value}`, {
+                    credentials: 'same-origin',
+                    method: 'POST',
+                    body: JSON.stringify({
+                        note: data.note,
+                    })
+                })
+                    .then((res) => res.json())
+                    .then((result) => {
+                        result?.data && onUpdate && onUpdate({
+                            data: result.data,
+                            action: 'save',
+                        });
+                    })
+                    .catch((e) => {
+                        setErrorMessage('Error saving data, see console error log for details.');
+                        logError(`Error sending: /api/action/save/${value}`, e);
+                    })
+                    .finally(() => setEditing(false));
+
+            } else {
+                setEditing(false);
+            }
+        }
 
         return (
             <div style={{paddingLeft: '3em'}}>
-                <div className="expandedNav">
-                    <div className="left">
-                        {showClaim && (<ActionButton label="Claim" id={data.id}/>)}
-                        {showChange && (<ActionButton label="Request Change" id={data.id}/>)}
-                        {showApprove && (<ActionButton label="Approve" id={data.id}/>)}
-                        <ActionButton label="Close" id={data.id}/>
-                        <ActionButton label="Remove" id={data.id}/>
+                {!editing && (
+                    <div>
+                        <div className="expandedNav">
+                            <div className="left">
+                                {showClaim && (<ActionButton label="Claim" id={data.id}/>)}
+                                {showChange && (<ActionButton label="Request Change" id={data.id}/>)}
+                                {showApprove && (<ActionButton label="Approve" id={data.id}/>)}
+                                <ActionButton label="Close" id={data.id}/>
+                                <ActionButton label="Remove" id={data.id}/>
+                                <button onClick={() => setEditing(true)}>Edit</button>
+                                {errorMessage && (<span className="error">{errorMessage}</span>)}
+                            </div>
+                            <div className="right">Last updated on {format(new Date(data.updatedAt), 'MMM dd, yyyy hh:mmaaaaa')}</div>
+                        </div>
+                        <div>{data.note}</div>
                     </div>
-                    <div className="right">Last updated on {format(new Date(data.updatedAt), 'MMM dd, yyyy hh:mmaaaaa')}</div>
-                </div>
-                <div>{data.note}</div>
+                )}
+                {editing && (
+                    <form id={`edit-${data.id}`}>
+                        <div><label htmlFor={`edit-note-${data.id}`}><strong>Notes</strong>:</label></div>
+                        <textarea
+                            id="{`edit-note-${data.id}`}" row={3} cols={80}
+                            defaultValue={data.note}
+                            onChange={({target}) => dirtyData.note = target.value}
+                        ></textarea>
+                        <div>
+                            <button type="button" onClick={() => setEditing(false)} id={`cancel-${data.id}`}>Cancel</button>
+                            <button type="button" onClick={handleSaveClicked} id={`save-${data.id}`} value={data.id}>Save</button>
+                        </div>
+                    </form>
+                )}
             </div>
         );
     };
 
-    return expandedRowComponent;
+    return ExpandedRowComponent;
 }
