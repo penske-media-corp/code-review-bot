30250a82e9bbe78bbb831f269c00de08e246fb04
Hau Vong <hvong@pmc.com>
2023-03-10 21:38:25 -0800

    eslint

--- src/express/middlewares/auth.ts
+++ src/express/middlewares/auth.ts
@@ -1,9 +1,9 @@
 import type {RequestHandler} from 'express';
 
 export const enforceAuthentication: RequestHandler = (req, res, next) => {
-    const user = req.user as {sid: string; fn: string};
+    const user = req.user as {sid?: string};
 
-    if (!user?.sid) {
+    if (!user.sid) {
         res.json({
             error: 'Not authorized',
         });
