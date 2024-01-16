7fa7e688453237e0adc4c7b3c892083d3d928c2f
Hau Vong <hvong@pmc.com>
2024-01-16 15:00:53 -0800

    Remove not yet in use extra field

--- prisma/migrations/20240116225639_add_github_id_to_user/migration.sql
+++ prisma/migrations/20240116225639_add_github_id_to_user/migration.sql
@@ -1,6 +1,3 @@
 -- AlterTable
-ALTER TABLE `code_reviews` ADD COLUMN `extra` JSON NULL;
-
--- AlterTable
 ALTER TABLE `users` ADD COLUMN `github_id` VARCHAR(40) NULL,
     MODIFY `slack_user_id` VARCHAR(25) NOT NULL;
